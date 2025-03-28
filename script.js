5adc40f8d483ec7ff8d5ee343b7e8389cd4fffc6
xinhanlim <xhxhan00@gmail.com>
2025-03-28 13:41:47 +0800

    added a map and container

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert('hellllllll')
+window.addEventListener('load', function(){
+    let singapore = [1.29, 103.85];
+    let map = L.map("mapContainer"). setView(singapore, 13);
+
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom:19,
+        attribution:
+        '&copy; <a href="httpL//www.openstreetmap.org/copyright">OpenstreetMap</a>',
+    }).addTo(map);
+})
