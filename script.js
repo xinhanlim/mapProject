8faf63f06865cad8263774dbf94282fbc2a471ac
xinhanlim <xhxhan00@gmail.com>
2025-03-30 17:36:55 +0800

    remove leaflet control zoom on the left to the right

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 window.addEventListener('load', function(){
     let singapore = [1.29, 103.85];
-    let map = L.map("mapContainer"). setView(singapore, 13);
+    let map = L.map("mapContainer", {zoomControl:false}). setView(singapore, 13);
 
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
         maxZoom:19,
         attribution:
         '&copy; <a href="httpL//www.openstreetmap.org/copyright">OpenstreetMap</a>',
     }).addTo(map);
+    
+    // Add zoom control manually to the top right
+    L.control.zoom({
+        position: 'topright'
+    }).addTo(map);
+    
 })
